67611e27d082df4348879bf53c208d95a08f99f0
Jonas Dahl <jonas.dahl@yanzinetworks.com>
2022-01-07 15:47:10 +0100

    Remove K

--- src/home-assistant/components/device.ts
+++ src/home-assistant/components/device.ts
@@ -24,6 +24,6 @@ export async function getDeviceConfig({
     via_device: unit.gatewayDid,
     sw_version: unit.version ?? undefined,
     suggested_area:
-      unit.chassisParent?.unitTypeFixed === UnitType.Gateway ? unit.name : unit.assetParent?.name ?? undefined,
+      unit.chassisParent?.unitTypeFixed === UnitType.Gateway ? undefined : unit.assetParent?.name ?? undefined,
   };
 }
